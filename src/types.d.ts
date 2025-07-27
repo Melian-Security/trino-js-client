60625a155e28a145b9ce8c8ef45bb0e551d5978a
matanper <matanper@gmail.com>
2025-07-27 20:51:59 +0300

    ignore .vscode

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'lz4-napi' {
-  export function uncompressSync(input: Buffer, maxOutputSize?: number): Buffer;
-  export function compressSync(input: Buffer): Buffer;
-  export function decompressFrameSync(input: Buffer): Buffer;
-  export function compressFrameSync(input: Buffer): Buffer;
-} 
